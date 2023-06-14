302d509fcdc27fd467012315cb236d4143db5492
NataliaMelissa <u201816996@upc.edu.pe>
2023-06-14 14:29:57 -0500

    Actualizar archivos

--- Ejercicios Clases/Ejercicio/ResourceManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "ResourceManager.h"
-
-TextureCache ResourceManager::textureCache;
-
-//Método: Obtener la textura en base a su TexturePath/dirección
-GLTexture ResourceManager::getTexture(string texturePath) {
-	return textureCache.getTexture(texturePath);
-}
