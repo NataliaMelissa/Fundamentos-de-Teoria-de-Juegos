0272f56f4b0f77a48fd8b45da61f3dac6b14cc93
NataliaMelissa <u201816996@upc.edu.pe>
2023-06-16 12:58:27 -0500

    Actualizar colores humanos y jugador

--- TA3/TA3/Player.cpp
+++ TA3/TA3/Player.cpp
@@ -11,11 +11,11 @@ Player::~Player()
 
 void Player::init(float speed, glm::vec2 position, InputManager* inputManager)
 {
-	this->path = "Textures/circle.png";
-	this->speed = speed;
-	this->position = position;
-	this->inputManager = inputManager;
-	color.set(185, 0, 0, 255);
+	this->path = "Textures/circle.png"; //Camino de la textura
+	this->speed = speed; //Asignarle la Velocidad
+	this->position = position; //Asignarle laPosición
+	this->inputManager = inputManager; //Asignarle el Input manager
+	color.set(2, 250, 213, 255); //Asignarle el color celeste/cyan al jugador
 }
 
 void Player::update(const vector<string>& levelData, vector<Human*>& humans, vector<Zombie*>& zombies)
