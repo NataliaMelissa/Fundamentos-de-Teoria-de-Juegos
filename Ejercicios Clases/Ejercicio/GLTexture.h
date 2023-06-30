f01ec3968bf8e8a162c63da2818cba239cd96b32
NataliaMelissa <u201816996@upc.edu.pe>
2023-06-29 23:08:28 -0500

    Crear y Configurar Archivo

--- Ejercicios Clases/Ejercicio/GLTexture.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <GL/eglew.h>
-
-struct GLTexture {
-	GLuint id; //ID textura
-	float width; //Ancho textura
-	float height; //Alto textura
-};
--- TA/TA1/TA1/Error.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-using namespace std;
-
-extern void FatalError(string errorString);
